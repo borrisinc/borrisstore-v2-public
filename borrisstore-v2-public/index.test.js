4702b9dbcd7e732da4014cf06455b15f7c07929b
hi5a2 <95431372+hi5a2@users.noreply.github.com>
2022-01-30 21:34:23 -0500

    Update index.test.js

--- borrisstore-v2-public/index.test.js
+++ borrisstore-v2-public/index.test.js
@@ -7,10 +7,13 @@ const fs = require("fs");
 const path = require("path");
 
 const html = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
+const html2 = fs.readFileSync(path.resolve(__dirname, "./about.html"), "utf8");
+const html3 = fs.readFileSync(path.resolve(__dirname, "./buy.html"), "utf8");
+const html4 = fs.readFileSync(path.resolve(__dirname, "./copyright.html"), "utf8");
 
 let dom;
 let container;
-
+//home page
 describe("Test for elements on Home page", () => {
   beforeEach(() => {
     dom = new JSDOM(html, { runScripts: "dangerously" });
@@ -24,3 +27,54 @@ describe("Test for elements on Home page", () => {
     ).toBeInTheDocument();
   });
 });
+
+
+//about page
+describe("Test for elements on About page", () => {
+  beforeEach(() => {
+    dom = new JSDOM(html2, { runScripts: "dangerously" });
+    container = dom.window.document.body;
+  });
+
+  it("Page renders a heading element", () => {
+    expect(container.querySelector("h1")).not.toBeNull();
+    expect(
+      getByText(container, "about the Borris company")
+    ).toBeInTheDocument();
+  });
+});
+
+
+//buy page
+describe("Test for elements on Buy page", () => {
+  beforeEach(() => {
+    dom = new JSDOM(html3, { runScripts: "dangerously" });
+    container = dom.window.document.body;
+  });
+
+  it("Page renders a heading element", () => {
+    expect(container.querySelector("h1")).not.toBeNull();
+    expect(
+      getByText(container, "green")
+    ).toBeInTheDocument();
+  });
+});
+
+
+
+//copyright page
+describe("Test for elements on Copyright page", () => {
+  beforeEach(() => {
+    dom = new JSDOM(html4, { runScripts: "dangerously" });
+    container = dom.window.document.body;
+  });
+
+  it("Page renders a heading element", () => {
+    expect(container.querySelector("h1")).not.toBeNull();
+    expect(
+      getByText(container, "green")
+    ).toBeInTheDocument();
+  });
+});
+
+//hint
