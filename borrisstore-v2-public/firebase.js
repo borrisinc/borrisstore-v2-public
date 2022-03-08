da1d480dbb909d154e8e0e0f2b744fbf678e9d81
hi5a <okbomer999@gmail.com>
2022-03-08 05:05:03 +0000

    yeet

---
+++ borrisstore-v2-public/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAOipfOxBznFwmOqCGImrpYTiV6XinCyLw",
+  authDomain: "borris-store.firebaseapp.com",
+  projectId: "borris-store",
+  storageBucket: "borris-store.appspot.com",
+  messagingSenderId: "845244285365",
+  appId: "1:845244285365:web:fe583c64c2450f92a759fc",
+  measurementId: "G-B1QCBPLX0Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
