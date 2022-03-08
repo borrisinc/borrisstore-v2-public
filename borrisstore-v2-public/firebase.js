2c22388b2f9d6090ca083a8554970c4941c9681a
hi5a <okbomer999@gmail.com>
2022-03-08 23:58:29 +0000

    firebase auth

--- borrisstore-v2-public/firebase.js
+++ borrisstore-v2-public/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "firebase/firestore";
+import {getDatabase} from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +22,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+const database = getDatabase(app);
