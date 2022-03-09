e1a320b312cbe4e57e4675456d8b594284c1269f
hi5a <okbomer999@gmail.com>
2022-03-09 00:11:39 +0000

    iuwefhywofhw

--- borrisstore-v2-public/login.js
+++ borrisstore-v2-public/login.js
@@ -1,29 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from "firebase/firestore";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAOipfOxBznFwmOqCGImrpYTiV6XinCyLw",
-  authDomain: "borris-store.firebaseapp.com",
-  projectId: "borris-store",
-  storageBucket: "borris-store.appspot.com",
-  messagingSenderId: "845244285365",
-  appId: "1:845244285365:web:fe583c64c2450f92a759fc",
-  measurementId: "G-B1QCBPLX0Z"
-};
+var firebaseConfig = {
+    apiKey: "AIzaSyAOipfOxBznFwmOqCGImrpYTiV6XinCyLw",
+    authDomain: "borris-store.firebaseapp.com",
+    projectId: "borris-store",
+    storageBucket: "borris-store.appspot.com",
+    messagingSenderId: "845244285365",
+    appId: "1:845244285365:web:fe583c64c2450f92a759fc"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // Initialize variables
 const auth = firebase.auth()
